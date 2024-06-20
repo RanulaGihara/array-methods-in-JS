7518c2518493e2c4418c9d197182f3c42db210c8
ranulagihara <ranulaw@opusxenta.com>
2024-06-21 01:39:30 +0530

    foreach

---
+++ foreach.js
@@ -0,0 +1,24 @@
+//generate me a array object which has name and age
+
+
+const users = [
+    {
+      name: "harry",
+      age: 19,
+    },
+    {
+      name: "larry",
+      age: 18,
+    },
+    {
+      name: "larry",
+      age: 18,
+    },
+    { name: "larry", age: 18 },
+  ];
+  
+
+  users.forEach((user) => {
+    console.log(user.name);
+  });
+  
